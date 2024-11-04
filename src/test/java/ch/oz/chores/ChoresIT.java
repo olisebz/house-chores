54cfe813993619301fae7d24961a63860ed0db99
Oliver Zenger <oliver@zenger.org>
2024-11-04 15:01:39 +0000

    Add initial Quarkus application setup with REST endpoint and Docker support

---
+++ src/test/java/ch/oz/chores/ChoresIT.java
@@ -0,0 +1,8 @@
+package ch.oz.chores;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ChoresIT extends ChoresTest {
+    // Execute the same tests but in packaged mode.
+}
